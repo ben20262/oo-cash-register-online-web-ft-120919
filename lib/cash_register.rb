5b7d04c2529dd2ae83211223218b81a1dba1e6d5
Benjamin Small <ben20262@aim.com>
2019-12-18 23:38:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :total, :discount
   @@cart = []
 
-  def initialize(discount = 0)
+  def initialize(discount = 20)
     @total = 0
     @discount = discount
   end
