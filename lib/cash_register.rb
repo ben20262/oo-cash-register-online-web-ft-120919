023264c2aef950eff19758bb68b40e67dabc582a
Benjamin Small <ben20262@aim.com>
2019-12-18 23:46:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total *= (@discount / 100.0)
+    @total *= (1 - (@discount / 100.0))
     "After the discount, the total comes to $#{@total}."
   end
 
